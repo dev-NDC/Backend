a293adeb16f7ef015bcfec7d34b418e1526e6006
Ved Prakash <vedprakash182001@gmail.com>
2025-05-07 00:17:10 +0530

    database

--- database/db.js
+++ database/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-const MONGO_URI = "mongodb://127.0.0.1:27017/NDC";
+require('dotenv').config();
+const MONGO_URI = process.env.MONGO_URI
 
 const connectDB = async () => {
   try {
